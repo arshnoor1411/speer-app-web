7d820648c7ffb960f2415bd1658012d1e47e6a7f
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2024-01-08 23:15:13 +0530

    enhance: removed header

--- src/components/Header.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "../styles/Header.module.css";
-
-const Header = () => {
-  return <header className={styles.header}>This is the header</header>;
-};
-
-export default Header;
