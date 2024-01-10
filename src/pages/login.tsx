ea9e501fa2e83adccaf4416b1c2f56d002a59595
Arshnoor Singh <32287091+arshnoor1411@users.noreply.github.com>
2024-01-10 19:01:43 +0530

    fix: moved business logic to componets folder of respective pages

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,25 +3,10 @@ import axios from "axios";
 import { useState } from "react";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleLogin = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-
-    const repsonse = await axios.post("http://localhost:3000/auth/login", {
-      email,
-      password,
-    });
-  };
   return (
-    <LoginForm
-      email={email}
-      password={password}
-      setEmail={setEmail}
-      setPassword={setPassword}
-      onSubmit={handleLogin}
-    ></LoginForm>
+    <div>
+      <LoginForm />
+    </div>
   );
 };
 export default Login;
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -4,42 +4,7 @@ import axios from "axios";
 import { useState } from "react";
 
 const SignUp = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const [otp, setOtp] = useState("");
-  const [showOtpPopup, setShowOtpPopup] = useState(false);
-
   const [signedUp, setSignedUp] = useState(false);
-
-  const handleSignup = async (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-
-    const response = await axios.post("http://localhost:4000/auth/signup", {
-      firstName,
-      lastName,
-      email,
-      password,
-    });
-    console.log("RRR", response);
-
-    if (response.data) {
-      console.log("Successfully Signed Up");
-    } else {
-      // Handle error scenarios
-      console.error("User registration failed:", response.data.error);
-    }
-  };
-
-  const handleOtpClose = () => {
-    // Close the OTP popup
-    setShowOtpPopup(false);
-    // Set the user as signed up
-    setSignedUp(true);
-  };
 
   return (
     <div>
@@ -49,20 +14,9 @@ const SignUp = () => {
           <p>You are now signed up.</p>
         </div>
       ) : (
-        <SignupPage
-          firstName={firstName}
-          lastName={lastName}
-          email={email}
-          password={password}
-          otp={otp}
-          setFirstName={setFirstName}
-          setLastName={setLastName}
-          setEmail={setEmail}
-          setPassword={setPassword}
-          setOtp={setOtp}
-          onSubmit={handleSignup}
-        />
-        //{showOtpPopup && <OtpForm onClose={handleOtpClose} />}
+        <div>
+          <SignupPage />
+        </div>
       )}
     </div>
   );
